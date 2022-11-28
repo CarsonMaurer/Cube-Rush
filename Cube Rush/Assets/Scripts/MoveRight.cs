736aea8f5c5aedbb5a429961b7b772cc1bb98a74
CarsonMaurer <111007062+CarsonMaurer@users.noreply.github.com>
2022-11-28 11:31:33 -0800

    added jumping, background

---
+++ Cube Rush/Assets/Scripts/MoveRight.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveLeft : MonoBehaviour
+{
+    private PlayerController _playerScript;
+    public float moveSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerScript = GameObject.Find("Player").GetComponent<PlayerController>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_playerScript.isGameover == false)
+        {
+             transform.Translate(Vector2.right * moveSpeed *Time.deltaTime);
+        }
+       
+    }
+}
